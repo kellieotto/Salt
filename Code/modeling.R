c9480c96a2f475a5913b786ddfc58fbc5266219b
kellieotto <kellieotto@berkeley.edu>
2015-02-03 14:05:22 -0800

    Implemented tuning of parameters and CV using caret

--- Code/modeling.R
+++ Code/modeling.R
@@ -1,25 +1,34 @@
-library(rpart)
-library(ipred)
-library(randomForest)
-library(e1071)
-library(gam)
-library(gbm)
-library(nnet)
-choose_model <- function(dat){
+# library(rpart)
+# library(ipred)
+# library(randomForest)
+# library(e1071)
+# library(gam)
+# library(gbm)
+# library(nnet)
+library(caret)
+choose_model <- function(dat, cv = 5){
   ### dat = dataframe of inputs, including the outcome and all predictors. e0 is the outcome
-  mod_lm <- lm(e0~., data = dat); mod_lm$fitted
+  ### cv  = number of folds for cross-validation (default 5)
+  ctrl <- trainControl(method = "cv", number = cv)
+  mod_lm <- train(e0~., data = dat, method = "leapSeq", trControl = ctrl, tuneLength = 10); predict(mod_lm, dat)
   mod_poly <- lm(dat$e0~poly(as.matrix(dat[,-1], degree = 2, raw = TRUE))); predict(mod_poly)
-  mod_tree <- rpart(e0~., data=dat); predict(mod_tree)
-  mod_bag <- bagging(e0~., data=dat); predict(mod_bag)
-  mod_rf <- randomForest(e0~., data=dat); predict(mod_rf)
-  mod_svm <- svm(e0~., data = dat)
-  mod_gam <- gam(e0~., data = dat)
-  mod_boost <- gbm(e0~., data = dat, bag.fraction = 1, train.fraction = 1)
-  mod_nnet <- nnet(e0/6 ~ ., data = dat, size = 2)
+  mod_tree <- train(e0~., data=dat, method = "rpart", trControl = ctrl, tuneLength = 10)
+  mod_bag <- train(e0~., data = dat, method = "treebag", tuneLength = 10, trControl = ctrl)
+  mod_rf <- train(e0~., data = dat, method = "rf", tuneLength = 16, trControl = ctrl)
+  mod_svm <- train(e0~., data = dat, method = "svmRadial", tuneGrid = expand.grid("C" = 2^seq(-2, 8),"sigma"=2*seq(.1,1, by = 1/10)), trControl = ctrl)
+  mod_boost <- train(e0~., data = dat, method = "gbm", tuneLength = 20, bag.fraction = 1, trControl = ctrl, verbose = FALSE)
+  mod_nnet <- train(e0~., data = dat, method = "nnet", tuneLength = 10, trControl = ctrl, verbose = FALSE)
+  
+  
+  
+  ### Can't seem to get GAMs to work?
+  #mod_gam <- gam(formula(paste("e0~", paste(sapply(colnames(male), function(x) paste("s(",x,")")), collapse = "+"))), data = dat)
+  #mod_gam <- train(e0~., data = dat, method = "gam", trControl = ctrl)
+  
+  
+  
   models <- list(mod_lm, mod_poly, mod_tree, mod_bag, mod_rf, mod_svm, mod_gam, mod_nnet, mod_boost)
-  mse <- sapply(models[1:length(models)-1], function(x) mean((predict(x) - dat$e0)^2))
-  mse <- c(mse, mean((predict(mod_nnet)*6 - dat$e0)^2))
-  mse <- c(mse, mean((predict(mod_boost, dat, n.trees = 100) - dat$e0)^2))
+  mse <- sapply(models[1:length(models)], function(x) mean((predict(x) - dat$e0)^2))
   best <- which.min(mse)[1]
   return(models[[best]])
 }
