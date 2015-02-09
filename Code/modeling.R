d7aa191ea5c95f5b1d778232604d3985c0fa4a9f
kellieotto <kellieotto@berkeley.edu>
2015-02-09 13:12:30 -0800

    Impute missing ETOH values

--- Code/modeling.R
+++ Code/modeling.R
@@ -12,27 +12,17 @@ library(caret)
 choose_model <- function(dat, bs = 10){
   ### dat = dataframe of inputs, including the outcome and all predictors. e0 is the outcome
   ### bs  = number of bootstrap replicates (default 10)
+  suppressWarnings({
   ctrl <- trainControl(method = "boot", number = bs)
-  mod_lm <- train(e0~., data = dat, method = "leapSeq", trControl = ctrl, tuneLength = 10)
+  mod_lm <- train(e0~., data = dat, method = "lm", trControl = ctrl, tuneLength = 10)
   mod_poly <- lm(dat$e0~poly(as.matrix(dat[,-1], degree = 2, raw = TRUE)))
   mod_tree <- train(e0~., data=dat, method = "rpart", trControl = ctrl, tuneLength = 10)
   mod_bag <- train(e0~., data = dat, method = "treebag", tuneLength = 10, trControl = ctrl)
-  mod_rf <- train(e0~., data = dat, method = "rf", tuneLength = 16, trControl = ctrl)
+  mod_rf <- train(e0~., data = dat, method = "rf", tuneLength =10, trControl = ctrl)
   mod_boost <- train(e0~., data = dat, method = "gbm", tuneLength = 20, bag.fraction = 1, trControl = ctrl, verbose = FALSE)
   mod_nnet <- train(e0/6~., data = dat, method = "nnet", tuneLength = 10, trControl = ctrl, trace = FALSE)
-  
-  ### This is a filler until I decide whether or not SVMs are appropriate
-  # mod_svm <- train(e0~., data = dat, method = "svmRadial", tuneGrid = expand.grid("C" = 2^seq(-2, 8),"sigma"=2*seq(.1,1, by = 1/10)), trControl = ctrl)
-  mod_svm <- lm(e0~alcohol, data=dat)
-  # this has really bad prediction error so it'll never get chosen.
-  
-  
-  ### Can't seem to get GAMs to work?
-  #mod_gam <- gam(e0~., data = dat)
-  #mod_gam <- gam(formula(paste("e0~", paste(sapply(colnames(male), function(x) paste("s(",x,")")), collapse = "+"))), data = dat)
-  #mod_gam <- train(y=dat$e0, x=dat[,-1], method = "gam", trControl = ctrl)
-  
-  
+  mod_svm <- train(e0~., data = dat, method = "svmRadial", tuneGrid = expand.grid("C" = 2^seq(-2, 8),"sigma"=2*seq(.1,1, by = 1/10)), trControl = ctrl)
+  })
   
   models <- list(mod_lm, mod_poly, mod_tree, mod_bag, mod_rf, mod_svm, mod_boost, mod_nnet)
   names(models) <- c("Linear regression", "Polynomial regression", "CART", "Bagged CART", "Random forest", "SVM with radial basis functions", "Stochastic gradient boosting", "Neural net")
