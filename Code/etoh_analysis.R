3aaf292aa5334b977488f59cf8482bbd8b35d375
kellieotto <kellieotto@berkeley.edu>
2015-03-14 09:55:07 -0700

    Add smoking and etoh analysis code

--- Code/etoh_analysis.R
+++ Code/etoh_analysis.R
@@ -1,127 +1,58 @@
-### Last edited: January 19, 2015
+### Last edited: March 16, 2015
 ### Kellie Ottoboni
-### Test for association between change in Na+ consumption and change in life expectancy, from 1990 to 2010
+### Test for association between change in etoh consumption and change in life expectancy, from 1990 to 2010
 
+set.seed(38)
 library(dplyr)
 library(Hmisc)
-detach(package:e1071)
 
 setwd("../Data")
 salt <- read.table("omnibus_data.csv", sep = "\t", header = TRUE)
 
 
 ### Clean up data, impute missing predictors within each year
-salt$alcohol_M   <- ifelse(is.na(salt$etohM), salt$etohboth, salt$etohM)
-salt$alcohol_F   <- ifelse(is.na(salt$etohF), salt$etohboth, salt$etohF)
-salt$alcohol_avg <- ifelse(is.na(salt$etohboth), 0.5*(salt$etohF+salt$etohM), salt$etohboth)
+salt <- mutate(salt, popF_prop = popF/(popF + popM), popM_prop = popM/(popF + popM))
+salt$popF_prop[is.na(salt$popF)] <- 0.5; salt$popM_prop[is.na(salt$popM)] <- 0.5
+salt$etohboth <- ifelse(is.na(salt$etohboth), salt$popM_prop*salt$etohM + salt$popF_prop*salt$etohF, salt$etohboth)
 
 salt <- arrange(salt, country, year)
-salt_filt <- select(salt, country, e0_M:rgdpe,emp:alcohol_avg)
-salt_filt <- select(salt_filt, -(etohboth:etohF), -pwt_year)
+salt_filt <- dplyr::select(salt, country, e0_M, e0_F, year, Na_M, Na_F, etohboth, etohM, etohF, pc_gdp, popM_prop, popF_prop, smoking_M, smoking_F)
 
 salt2010           <- filter(salt_filt, year == 2010)
-salt2010$avh       <- as.numeric(impute(salt2010$avh, median))
-salt2010$hc        <- as.numeric(impute(salt2010$hc, median))
-salt2010$ctfp      <- as.numeric(impute(salt2010$ctfp, median))
-salt2010$rtfpna    <- as.numeric(impute(salt2010$rtfpna, median))
-
+etohM_mod <- lm(etohM~ e0_M + e0_F + Na_M + Na_F + pc_gdp + smoking_M + smoking_F, data = salt2010)
+etohF_mod <- lm(etohF~ e0_M + e0_F + Na_M + Na_F + pc_gdp + smoking_M + smoking_F, data = salt2010)
+twn <- which(is.na(salt2010$etohM))
+salt2010[twn, "etohM"] <- predict(etohM_mod, salt2010[twn,]); salt2010[twn, "etohF"] <- predict(etohF_mod, salt2010[twn,])
+salt2010[twn, "etohboth"] <- (salt2010$popM_prop*salt2010$etohM + salt2010$popF_prop*salt2010$etohF)[twn]
 
 salt1990           <- filter(salt_filt, year == 1990)
-salt1990$avh       <- as.numeric(impute(salt1990$avh, median))
-salt1990$hc        <- as.numeric(impute(salt1990$hc, median))
-salt1990$ctfp      <- as.numeric(impute(salt1990$ctfp, median))
-salt1990$rtfpna    <- as.numeric(impute(salt1990$rtfpna, median))
+etohboth_mod <- lm(etohboth ~ e0_M + e0_F + Na_M + Na_F + pc_gdp + smoking_M + smoking_F, data = salt1990)
+twn <- which(is.na(salt1990$etohboth))
+salt1990[twn, "etohboth"] <- predict(etohboth_mod, salt1990[twn,])
+salt1990$etohM     <- salt1990$etohboth * (salt2010$etohM)/(salt2010$popM_prop*salt2010$etohM+salt2010$popF_prop*salt2010$etohF)
+salt1990$etohF     <- salt1990$etohboth * (salt2010$etohF)/(salt2010$popM_prop*salt2010$etohM+salt2010$popF_prop*salt2010$etohF)
 
 
 ### Take difference between 2010 and 1990, split into male and female.
 countries <- salt2010$country
 salt_diff <- salt2010[,-1]-salt1990[,-1]
 salt_diff <- cbind(countries, salt_diff)
-salt_diff <- filter(salt_diff, !is.na(e0_M) & !is.na(alcohol_M))
+salt_diff <- filter(salt_diff, !is.na(e0_M))
 
-male <- select(salt_diff, -countries, -e0_F, -year, -Na_F, -alcohol_M, -alcohol_F)
-male_etoh <- salt_diff$alcohol_M
+male <- dplyr::select(salt_diff, e0_M, pc_gdp, Na_M, smoking_M)
+male_etoh <- salt_diff$etohM
 colnames(male) <- gsub("_M", "", colnames(male))
-female <- select(salt_diff, -countries, -e0_M, -year, -Na_M, -alcohol_F, -alcohol_M)
-female_etoh <- salt_diff$alcohol_F
+female <- dplyr::select(salt_diff, e0_F, Na_F, smoking_F, pc_gdp)
+female_etoh <- salt_diff$etohF
 colnames(female) <- gsub("_F", "", colnames(female))
 
-alc_M <- select(salt_diff, -countries, -e0_F, -Na_F, -year, -alcohol_F, -alcohol_M, -alcohol_avg)
-colnames(alc_M)[1] <- "e0"
-alc_F <- select(salt_diff, -countries, -e0_M, -Na_M, -year, -alcohol_F, -alcohol_M, -alcohol_avg)
-colnames(alc_F)[1] <- "e0"
-avg_etoh <- salt_diff$alcohol_avg
 
 
-############# Do it the right way?##################################################################
-### Modeling step - test a bunch of different models and pick the one with the best in-sample fit
-
-source("../Code/perm_tests.R")
-mod_M <- choose_model(alc_M)
-pred_M <- predict(mod_M, alc_M)
-mod_F <- choose_model(alc_F)
-pred_F <- predict(mod_F, alc_F)
-
-### Permutation test
-
-res_M <- permu_pearson(prediction = pred_M, outcome = alc_M$e0, treatment = avg_etoh, iters = 10000)
-res_F <- permu_pearson(prediction = pred_F, outcome = alc_F$e0, treatment = avg_etoh, iters = 10000)
-
-
-### Confidence intervals - by inverting the permutation test
-
-ci_M <- permu_CI_pearson(prediction = pred_M, outcome = alc_M$e0, treatment = avg_etoh, iters = 10000, side = "both")
-ci_F <- permu_CI_pearson(prediction = pred_F, outcome = alc_F$e0, treatment = avg_etoh, iters = 10000, side = "both")
-
-cat("MALE:\n", "Estimate:", res_M$estimate, "\nP-value", res_M$pvalue, "\nConfidence Interval", ci_M)
-cat("FEMALE:\n", "Estimate:", res_F$estimate, "\nP-value", res_F$pvalue, "\nConfidence Interval", ci_F)
-
-### Plots
-library(ggplot2)
-setwd("../Analysis")
-
-#pdf("etoh_lifeexp.pdf")
-dat <- data.frame("sex"     = c(rep("Male",nrow(alc_M)), rep("Female",nrow(alc_F))),
-                  "etoh"  = as.numeric(c(avg_etoh, avg_etoh)),
-                  "le"      = c(alc_M$e0, alc_F$e0),
-                  "country" = rep(salt_diff$countries, 2),
-                  "ex_mort" = c(pred_M-alc_M$e0, pred_F-alc_F$e0),
-                  stringsAsFactors = FALSE)
-qplot(etoh, le, data = dat, facets = sex~., colour = sex) + theme(legend.position = "none") + labs(x = "Alcohol Consumed (L/year)", y = "Life Expectancy (years)", title = "Change from 1990 to 2010") + geom_text(aes(label=ifelse(le<0,paste(country),"")) , hjust=-0.2, size = 3.5)
-dev.off()
-
-
-pdf("permdistribution_etoh.pdf")
-dat2 <- data.frame("sex"     = c(rep("Male",length(res_M$distr)), rep("Female",length(res_F$distr))),
-                   "distr"  = as.numeric(c(res_M$distr, res_F$distr)),
-                   stringsAsFactors = FALSE)
-vline.dat <- data.frame("sex" = c(rep("Male",3), rep("Female",3)),
-                        "vline"  = as.numeric(c(res_M$estimate, ci_M, res_F$estimate, ci_F)),
-                        "lty"   = rep(c("dashed", "dotted", "dotted"), 2),
-                        stringsAsFactors = FALSE)
-qplot(distr, data = dat2, geom = "histogram", facets = sex~., colour = sex, fill = sex) + geom_vline(aes(xintercept=vline, linetype=lty), data = vline.dat, show_guide = TRUE, color="black")+ theme(legend.position = "none") + labs(x = "Pearson Correlation", y = "", title = "Permutation Distribution")
-dev.off()
-
-pdf("etoh_exmort.pdf")
-qplot(etoh, ex_mort, data = dat, facets = sex~., colour = sex) + theme(legend.position = "none") + labs(x = "Ethanol (L/year)", y = "Excess Mortality (years)", title = "Change in Alcohol Consumption") + geom_text(aes(label=ifelse(ex_mort < -2 | ex_mort > 2, paste(country), "")), hjust=-0.2, size = 3.5)
-dev.off()
-
-pdf("lifeex_exmort_etohmodels.pdf")
-qplot(le, ex_mort, data = dat, facets = sex~., colour = sex) + theme(legend.position = "none") + labs(x = "Life Expectancy (years)", y = "Excess Mortality (years)", title = "Change in Life Expectancy")  + geom_text(aes(label=ifelse(ex_mort < -2 | ex_mort > 2, paste(country), "")), hjust=1.1, size = 3.5)
-dev.off()
-
-
-
-
-
-
-
-
-###################The original (wrong?) way###################################################################
 
 ### Modeling step - test a bunch of different models and pick the one with the best in-sample fit
 
-source("../Code/perm_tests.R")
+source("../Code/modeling.R")
+library(devtools); install_github("kellieotto/ModelMatch/ModelMatch"); library(ModelMatch)
 mod_M <- choose_model(male)
 pred_M <- predict(mod_M, male)
 mod_F <- choose_model(female)
@@ -130,17 +61,15 @@ pred_F <- predict(mod_F, female)
 
 ### Permutation test
 
-res_M <- permu_pearson(prediction = pred_M, outcome = male$e0, treatment = male_etoh, iters = 10000)
-res_F <- permu_pearson(prediction = pred_F, outcome = female$e0, treatment = female_etoh, iters = 10000)
+
+res_M <- permu_pearson(prediction = pred_M, response = male$e0, treatment = male_etoh, iters = 10000)
+res_F <- permu_pearson(prediction = pred_F, response = female$e0, treatment = female_etoh, iters = 10000)
 
 
-### Confidence intervals - by inverting the permutation test
 
-ci_M <- permu_CI_pearson(prediction = pred_M, outcome = male$e0, treatment = male_etoh, iters = 10000, side = "both")
-ci_F <- permu_CI_pearson(prediction = pred_F, outcome = female$e0, treatment = female_etoh, iters = 10000, side = "both")
-
-cat("MALE:\n", "Estimate:", res_M$estimate, "\nP-value", res_M$pvalue, "\nConfidence Interval", ci_M)
-cat("FEMALE:\n", "Estimate:", res_F$estimate, "\nP-value", res_F$pvalue, "\nConfidence Interval", ci_F)
+res_table <- data.frame(rbind(c(res_M$estimate, res_M$pvalue), c(res_F$estimate, res_F$pvalue)))
+rownames(res_table) <- c("Male", "Female"); colnames(res_table) <- c("Estimate", "Upper p", "Lower p", "Two-sided p")
+print(res_table)
 
 
 
@@ -155,28 +84,15 @@ dat <- data.frame("sex"     = c(rep("Male",nrow(male)), rep("Female",nrow(female
                   "country" = rep(salt_diff$countries, 2),
                   "ex_mort" = c(pred_M-male$e0, pred_F-female$e0),
                   stringsAsFactors = FALSE)
-qplot(etoh, le, data = dat, facets = sex~., colour = sex) + theme(legend.position = "none") + labs(x = "Alcohol Consumed (L/year)", y = "Life Expectancy (years)", title = "Change from 1990 to 2010") + geom_text(aes(label=ifelse(le<0,paste(country),"")) , hjust=-0.2, size = 3.5)
+qplot(etoh, le, data = dat, facets = sex~., colour = sex) + theme(legend.position = "none") + labs(x = expression(paste(Delta, "Alcohol (L/year)")), y = expression(paste(Delta, " Life Expectancy (years)")), title = "Change from 1990 to 2010") + geom_text(aes(label=ifelse(le<0,paste(country),"")) , hjust=-0.2, size = 3.5)
 dev.off()
 
 
-pdf("permdistribution_etoh.pdf")
-dat2 <- data.frame("sex"     = c(rep("Male",length(res_M$distr)), rep("Female",length(res_F$distr))),
-                   "distr"  = as.numeric(c(res_M$distr, res_F$distr)),
-                   stringsAsFactors = FALSE)
-vline.dat <- data.frame("sex" = c(rep("Male",3), rep("Female",3)),
-                        "vline"  = as.numeric(c(res_M$estimate, ci_M, res_F$estimate, ci_F)),
-                        "lty"   = rep(c("dashed", "dotted", "dotted"), 2),
-                        stringsAsFactors = FALSE)
-qplot(distr, data = dat2, geom = "histogram", facets = sex~., colour = sex, fill = sex) + geom_vline(aes(xintercept=vline, linetype=lty), data = vline.dat, show_guide = TRUE, color="black")+ theme(legend.position = "none") + labs(x = "Pearson Correlation", y = "", title = "Permutation Distribution")
-dev.off()
 
 pdf("etoh_exmort.pdf")
-qplot(etoh, ex_mort, data = dat, facets = sex~., colour = sex) + theme(legend.position = "none") + labs(x = "Ethanol (L/year)", y = "Excess Mortality (years)", title = "Change in Alcohol Consumption") + geom_text(aes(label=ifelse(ex_mort < -2 | ex_mort > 2, paste(country), "")), hjust=-0.2, size = 3.5)
+qplot(etoh, ex_mort, data = dat, facets = sex~., colour = sex) + theme(legend.position = "none") + labs(x = expression(paste(Delta, "Alcohol (L/year)")), y = "Excess Mortality (years)", title = "Change from 1990 to 2010") + geom_text(aes(label=ifelse(ex_mort < -2, paste(country), "")), hjust=-0.2, size = 3.5)
 dev.off()
 
-pdf("lifeex_exmort_etohmodels.pdf")
-qplot(le, ex_mort, data = dat, facets = sex~., colour = sex) + theme(legend.position = "none") + labs(x = "Life Expectancy (years)", y = "Excess Mortality (years)", title = "Change in Life Expectancy")  + geom_text(aes(label=ifelse(ex_mort < -2 | ex_mort > 2, paste(country), "")), hjust=1.1, size = 3.5)
+pdf("lifeex_exmort_etohanalysis.pdf")
+qplot(le, ex_mort, data = dat, facets = sex~., colour = sex) + theme(legend.position = "none") + labs(x = expression(paste(Delta, " Life Expectancy (years)")), y = "Excess Mortality (years)", title = "Change in Life Expectancy")  + geom_text(aes(label=ifelse(ex_mort < -2, paste(country), "")), hjust=1.1, size = 3.5)
 dev.off()
-
-
-
--- Code/tobacco_analysis.R
+++ Code/tobacco_analysis.R
@@ -1,6 +1,6 @@
 ### Last edited: March 14, 2015
 ### Kellie Ottoboni
-### Test for association between change in Na+ consumption and change in life expectancy, from 1990 to 2010
+### Test for association between change in tobacco use and change in life expectancy, from 1990 to 2010
 
 set.seed(38)
 library(dplyr)
@@ -39,11 +39,11 @@ salt_diff <- salt2010[,-1]-salt1990[,-1]
 salt_diff <- cbind(countries, salt_diff)
 salt_diff <- filter(salt_diff, !is.na(e0_M))
 
-male <- dplyr::select(salt_diff, e0_M, etohM, smoking_M, pc_gdp)
-male_Na <- salt_diff$Na_M
+male <- dplyr::select(salt_diff, e0_M, etohM, pc_gdp)
+male_smoking <- salt_diff$smoking_M
 colnames(male) <- gsub("_M", "", colnames(male))
-female <- dplyr::select(salt_diff,  e0_F, etohF, smoking_F, pc_gdp)
-female_Na <- salt_diff$Na_F
+female <- dplyr::select(salt_diff, e0_F, etohF, pc_gdp)
+female_smoking <- salt_diff$smoking_F
 colnames(female) <- gsub("_F", "", colnames(female))
 
 
@@ -62,19 +62,13 @@ pred_F <- predict(mod_F, female)
 ### Permutation test
 
 
-res_M <- permu_pearson(prediction = pred_M, response = male$e0, treatment = male_Na, iters = 10000)
-res_F <- permu_pearson(prediction = pred_F, response = female$e0, treatment = female_Na, iters = 10000)
-
-
-### Confidence intervals - by inverting the permutation test
+res_M <- permu_pearson(prediction = pred_M, response = male$e0, treatment = male_smoking, iters = 10000)
+res_F <- permu_pearson(prediction = pred_F, response = female$e0, treatment = female_smoking, iters = 10000)
 
 
 
-ci_M <- permu_CI_pearson(prediction = pred_M, response = male$e0, treatment = male_Na, iters = 10000, side = "both", verbosity = TRUE)
-ci_F <- permu_CI_pearson(prediction = pred_F, response = female$e0, treatment = female_Na, iters = 10000, side = "both", verbosity = TRUE)
-
-res_table <- data.frame(rbind(c(res_M$estimate, res_M$pvalue, ci_M), c(res_F$estimate, res_F$pvalue, ci_F)))
-rownames(res_table) <- c("Male", "Female"); colnames(res_table) <- c("Estimate", "Upper p", "Lower p", "Two-sided p", "Lower CI", "Upper CI")
+res_table <- data.frame(rbind(c(res_M$estimate, res_M$pvalue), c(res_F$estimate, res_F$pvalue)))
+rownames(res_table) <- c("Male", "Female"); colnames(res_table) <- c("Estimate", "Upper p", "Lower p", "Two-sided p")
 print(res_table)
 
 
@@ -83,33 +77,24 @@ print(res_table)
 library(ggplot2)
 setwd("../Analysis")
 
-pdf("sodium_lifeexp.pdf")
+pdf("smoking_lifeexp.pdf")
 dat <- data.frame("sex"     = c(rep("Male",nrow(male)), rep("Female",nrow(female))),
-                  "sodium"  = as.numeric(c(male_Na, female_Na)),
+                  "smoking"  = as.numeric(c(male_smoking, female_smoking)),
                   "le"      = c(male$e0, female$e0),
                   "country" = rep(salt_diff$countries, 2),
                   "ex_mort" = c(pred_M-male$e0, pred_F-female$e0),
                   stringsAsFactors = FALSE)
-qplot(sodium, le, data = dat, facets = sex~., colour = sex) + theme(legend.position = "none") + labs(x = expression(paste(Delta, " Sodium (g/day)")), y = expression(paste(Delta, " Life Expectancy (years)")), title = "Change from 1990 to 2010") + geom_text(aes(label=ifelse(le<0,paste(country),"")) , hjust=-0.2, size = 3.5)
+qplot(smoking, le, data = dat, facets = sex~., colour = sex) + theme(legend.position = "none") + labs(x = expression(paste(Delta, " Smoking (cigarettes/smoker/day)")), y = expression(paste(Delta, " Life Expectancy (years)")), title = "Change from 1990 to 2010") + geom_text(aes(label=ifelse(le<0,paste(country),"")) , hjust=-0.2, size = 3.5)
 dev.off()
 
 
-pdf("permdistribution.pdf")
-dat2 <- data.frame("sex"     = c(rep("Male",length(res_M$distr)), rep("Female",length(res_F$distr))),
-                  "distr"  = as.numeric(c(res_M$distr, res_F$distr)),
-                  stringsAsFactors = FALSE)
-vline.dat <- data.frame("sex" = c(rep("Male",3), rep("Female",3)),
-                        "vline"  = as.numeric(c(res_M$estimate, ci_M, res_F$estimate, ci_F)),
-                        "lty"   = rep(c("dashed", "dotted", "dotted"), 2),
-stringsAsFactors = FALSE)
-qplot(distr, data = dat2, geom = "histogram", facets = sex~., colour = sex, fill = sex) + geom_vline(aes(xintercept=vline, linetype=lty), data = vline.dat, show_guide = TRUE, color="black")+ theme(legend.position = "none") + labs(x = "Pearson Correlation", y = "", title = "Permutation Distribution")
+
+pdf("smoking_exmort.pdf")
+qplot(smoking, ex_mort, data = dat, facets = sex~., colour = sex) + theme(legend.position = "none") + labs(x = expression(paste(Delta, " Smoking (cigarettes/smoker/day)")), y = "Excess Mortality (years)", title = "Change from 1990 to 2010") + geom_text(aes(label=ifelse(ex_mort < -2, paste(country), "")), hjust=-0.2, size = 3.5)
 dev.off()
 
-pdf("sodium_exmort.pdf")
-qplot(sodium, ex_mort, data = dat, facets = sex~., colour = sex) + theme(legend.position = "none") + labs(x = expression(paste(Delta, " Sodium (g/day)")), y = "Excess Mortality (years)", title = "Change in Sodium") + geom_text(aes(label=ifelse(ex_mort < -2, paste(country), "")), hjust=-0.2, size = 3.5)
-dev.off()
-
-pdf("lifeex_exmort.pdf")
+pdf("lifeex_exmort_smokinganalysis.pdf")
 qplot(le, ex_mort, data = dat, facets = sex~., colour = sex) + theme(legend.position = "none") + labs(x = expression(paste(Delta, " Life Expectancy (years)")), y = "Excess Mortality (years)", title = "Change in Life Expectancy")  + geom_text(aes(label=ifelse(ex_mort < -2, paste(country), "")), hjust=1.1, size = 3.5)
 dev.off()
+
 
