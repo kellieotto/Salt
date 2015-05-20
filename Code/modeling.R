a4f9106fafa9f7bf4b9c594ee6282753d03c2ce4
Kellie Ottoboni <kellieotto@berkeley.edu>
2015-05-20 10:45:42 -0700

    Remove neural nets; didnt make sense

--- Code/modeling.R
+++ Code/modeling.R
@@ -20,14 +20,12 @@ choose_model <- function(dat, bs = 10){
   mod_bag <- train(e0~., data = dat, method = "treebag", tuneLength = 10, trControl = ctrl)
   mod_rf <- train(e0~., data = dat, method = "rf", tuneLength =10, trControl = ctrl)
   mod_boost <- train(e0~., data = dat, method = "gbm", tuneLength = 20, bag.fraction = 1, trControl = ctrl, verbose = FALSE)
-  mod_nnet <- train(e0/6~., data = dat, method = "nnet", tuneLength = 10, trControl = ctrl, trace = FALSE)
   })
   
-  models <- list(mod_lm, mod_poly, mod_tree, mod_bag, mod_rf, mod_boost, mod_nnet)
-  names(models) <- c("Linear regression", "Polynomial regression", "CART", "Bagged CART", "Random forest", "Stochastic gradient boosting", "Neural net")
+  models <- list(mod_lm, mod_poly, mod_tree, mod_bag, mod_rf, mod_boost)
+  names(models) <- c("Linear regression", "Polynomial regression", "CART", "Bagged CART", "Random forest", "Stochastic gradient boosting")
   mse_calc <- function(x) mean((predict(x) - dat$e0)^2)
-  mse <- sapply(models[1:length(models)-1], mse_calc)
-  mse <- c(mse, mean((predict(mod_nnet)*6 - dat$e0)^2))
+  mse <- sapply(models, mse_calc)
   names(mse) <- names(models)
   best <- which.min(mse)[1]
   print(mse)
